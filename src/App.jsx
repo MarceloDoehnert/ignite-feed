74761e2ea18c91055ba0aca0c5722e9c7d584077
MarceloDoehnert <59656981+MarceloDoehnert@users.noreply.github.com>
2022-09-26 19:33:21 -0300

    inicio do feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import "./global.css"
+
+// Componentes
+import { Header } from "./components/Header"
+
 export function App() {
 
   return (
     <div className="App">
+      <Header />
     </div>
   )
 }
