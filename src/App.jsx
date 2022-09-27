a5874c3d8459a70980bd84c2f890738825674c3a
MarceloDoehnert <59656981+MarceloDoehnert@users.noreply.github.com>
2022-09-26 21:30:11 -0300

    sidebar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+// Styles
 import "./global.css"
+import styles from "./App.module.css"
 
 // Componentes
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
 
   return (
     <div className="App">
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <div>
+            aaaaaaaaa
+          </div>
+        </main>
+      </div>
+      
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import { PencilSimpleLine } from "phosphor-react"
+
+import styles from "./Sidebar.module.css"
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1659935405041-1e76d8bd9de6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/59656981?v=4" />
+
+        <strong>Marcelo Doehnert Cordeiro</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilSimpleLine />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
